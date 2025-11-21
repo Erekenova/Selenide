29ff4545c03d989a969b6960e2661b12cfb0c0b6
Albina Abzhetova <alba-madona@mail.ru>
2025-11-21 15:53:57 +0400

    2 тест готов

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -95,8 +95,46 @@ public class Demo extends BaseTest {
         }
     }
 
+    @Test
     public void test2() {
+        //  Кликнуть по меню "Widget"
+        $x("//h5[text()='Widgets']").click();
 
+        // На странице Widgets кликнуть по пункту "Progress Bar"
+        $x("//span[text()='Progress Bar']").click();
+        //  Проверить, что имеется кнопка с текстом "Start"
+        SelenideElement startButton = $x("//button[text()='Start']");
+        if (startButton.exists()) {
+            System.out.println("Кнопка 'Start' найдена");
+        } else {
+            System.out.println("Кнопка 'Start' не найдена");
+            return;
+        }
+        //  Кликнуть по кнопке "Start"
+        startButton.click();
+
+        // Проверить, что на форме имеется кнопка "Stop"
+        SelenideElement stopButton = $x("//button[text()='Stop']");
+        if (stopButton.exists()) {
+            System.out.println("Кнопка 'Stop' найдена");
+        } else {
+            System.out.println("Кнопка 'Stop' не найдена");
+            return;
+        }
+        //Когда прогресс бар достигнет 30% нажать "Stop"
+        SelenideElement progressValue = $x("//div[@role='progressbar']");
+        while (true) {
+            String progressText = progressValue.getText(); // например, "30%"
+            int progressPercent = Integer.parseInt(progressText.replace("%", "").trim());
+            if (progressPercent >= 30) {
+                stopButton.click();
+                System.out.println("Прогресс достиг 30%, нажата кнопка 'Stop'");
+                break;
+            }
+            sleep(500); // пауза 0.5 сек
+        }
     }
 
 }
+
+
